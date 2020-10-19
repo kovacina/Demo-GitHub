b061f5cf256171d79b9882dc3577782b8025dcaf
Peter T. Kovacina <pete.kovacina@gmail.com>
2020-10-18 21:47:59 -0400

    Initial Commit

---
+++ Demo-GitHub/Demo_GitHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  Demo_GitHubApp.swift
+//  Demo-GitHub
+//
+//  Created by Peter T. Kovacina on 10/18/20.
+//
+
+import SwiftUI
+
+@main
+struct Demo_GitHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
